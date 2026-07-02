6c4189d5da7d078a9d73a407c9141d55e82e8d17
Guiguilap <guilapalus@gmail.com>
2026-07-03 01:46:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -582,8 +582,8 @@ function Navbar({ user, onLogin, onSignup, onLogout, onDashboard, onHome, onShar
           </div>
         ) : (
           <>
-            <button className="btn btn-ghost" onClick={onLogin} style={{fontSize:10,padding:"7px 10px"}}>Connexion</button>
-            <button className="btn btn-primary" onClick={onSignup} style={{fontSize:10,padding:"7px 12px"}}>Commencer</button>
+          <button className="btn btn-ghost" onClick={onLogin} style={{fontSize:9,padding:"6px 8px"}}>Co</button>
+         <button className="btn btn-primary" onClick={onSignup} style={{fontSize:9,padding:"6px 8px"}}>Start</button>
           </>
         )}
       </div>
